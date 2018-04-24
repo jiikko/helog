91f25a840ca2763e5dbcc98383e256c69a9284b3
jiikko <n905i.1214@gmail.com>
2018-04-24 17:03:27 +0900

    add zgrep_by_params

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 clean:
 	rm -rf logs/* && rm -rf app_log
+clean_for_bin:
+	rm -rf output
---
+++ lib/helog/grep/by_params.rb
@@ -0,0 +1,83 @@
+require 'open3'
+require 'fileutils'
+
+module Helog
+  module Grep
+    class ByParams
+      def initialize(params, path)
+        @without_debug_log = true # params[:without_debug_log]
+        @patterns = params[:e]
+        @and_pattern = params[:and]
+        @paths = path.split(' ')
+
+        if @patterns.nil? || @patterns.empty?
+          puts 'input patterns!!'
+          exit 1
+        end
+        if path.nil?
+          raise('検索対象のファイルがありません')
+          exit 1
+        end
+      end
+
+      def run
+        patterns_for_cmd = @patterns.map { |x| " -e #{x} " }.join
+        cmd_after = []
+        if @without_debug_log
+          cmd_after << 'grep -v DEBUG | grep -v heroku | grep -v heroku'
+        end
+        if @and_pattern
+          cmd_after << "grep #{@and_pattern}"
+        end
+        string_cmd_after = cmd_after.map { |x| "| #{x} " }.join
+        cmd = "zgrep #{patterns_for_cmd} #{@paths.join(' ')} #{string_cmd_after}"
+
+        logfile_uuid_table = {}
+        Open3.popen2(cmd) do |_stdin, stdout, wait_thr|
+          puts "Executing... #{cmd}"
+          while line = stdout.gets
+            logfile = nil
+            uuid = nil
+            if /^([^:]+):/ =~ line
+              logfile = $1
+            else
+              puts 'logfile名の取得に失敗しました。なにかおかしいです'
+              puts "-> #{line}"
+            end
+            if %r!: (\[[-\w]+?\]) Started! =~ line
+              uuid = $1
+            else
+              puts 'uuidの取得に失敗しました。なにかおかしいです'
+              puts "-> #{line}"
+            end
+            logfile_uuid_table[logfile] ||= []
+            logfile_uuid_table[logfile] << uuid
+          end
+        end
+
+        begin
+          FileUtils.mkdir_p('output')
+          pids = []
+          logfile_uuid_table.each do |logfile, uuids|
+            u = uuids.map { |u| "-e '#{u}'" }.join(" ")
+            cmd = ("zgrep #{u} #{logfile}").gsub(']', '\]').gsub('[', '\[')
+            pids << fork do
+              Open3.popen2(cmd) do |_stdin, stdout, wait_thr|
+                puts "Executing... #{cmd}"
+                buffer = []
+                while line = stdout.gets
+                  buffer << line
+                end
+                File.write("./output/#{logfile.gsub('/', '_')}_by_params.log", buffer.join)
+                puts "finiesh!: #{logfile}"
+              end
+            end
+          end
+          pids.each { |pid| Process.wait(pid) }
+        rescue
+          pids.each { |pid| Process.kill(pid) }
+        end
+      end
+    end
+  end
+end
