d0e0d3f754532f8fde9e9afc79c7c1b07e0f24a5
jiikko <n905i.1214@gmail.com>
2018-10-29 10:38:55 +0900

    ログの分割サイズを環境変数から渡せるようにする

--- lib/helog.rb
+++ lib/helog.rb
@@ -5,7 +5,7 @@ require 'helog/fetcher'
 require 'helog/google_drive_uploader'
 
 module Helog
-  LOGGER_ROTATE_SIZE = 1000 * 1024 * 1024 # 1GB
+  LOGGER_ROTATE_SIZE = ENV['ROTATE_SIZE'] || 1000 * 1024 * 1024 # 1GB
 
   def self.run(cmd: , logfilename: )
     Runner.new(cmd, logfilename).run
